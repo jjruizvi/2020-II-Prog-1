7c151456330d2fa25501949a2c86a10ee20c247a
Iluvatar <woquendo@gmail.com>
2020-08-28 10:53:20 -0500

    Anhado ejemplo de funcion sin

--- 2020-08-28-git/ejemplo.cpp
+++ 2020-08-28-git/ejemplo.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+// Primer programa de ejemplo de c++
+/*
+  s,mfsdnfsdf sdf dsf ds fsdf
+  f sd fsdf dsf dsf dsfdsf ds sfdsd
+  sdfdsfds
+*/
+
+#include <iostream> // para escribir y leer de la libreria estandar
+#include <cmath> // inclur funciones matematicas
 
 int main(void)
 {
     std::cout << "Hello world!\n";
     std::cout << "Hola mundo!\n";
+    std::cout << "sin(90):" << std::sin(2.3) << "\n";
     return 0;
 }
