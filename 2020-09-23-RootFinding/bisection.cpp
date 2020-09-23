d06a5fc25df0d82594aed6400791587ff03403af
William Oquendo <wfoquendop@unal.edu.co>
2020-09-23 09:43:06 -0500

    Draft for bisection

---
+++ 2020-09-23-RootFinding/bisection.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using fptr = double(double);
+double f(double x);
+double bisection(double xl, double xu, double eps, fptr fun);
+
+int main(int argc, char **argv)
+{
+    std::cout.precision(15); std::cout.setf(std::ios::scientific);
+    double xl = 0;
+    double xu = 10.0;
+    double eps = 1.0e-1;
+
+    double xroot = bisection(xl, xu, eps, f);
+
+    std::cout << xroot << "\t" << f(xroot) << "\n";
+
+    return 0;
+}
+
+double f(double x)
+{
+    const double G = 9.81;
+    const double M = 68.1;
+    const double T = 10.0;
+    const double VF = 40.0;
+    return (M*G*(1 - std::exp(-x*T/M))/x) - VF;
+}
+double bisection(double xl, double xu, double eps, fptr fun)
+{
+    
+}
